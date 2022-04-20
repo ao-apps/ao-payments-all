e6c3ff039c55cd9f7e636ee6c5f2b1fdb1838eb0
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:48:31 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * ao-payments-all - Convenience POM to include the payment processing API and all providers.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,11 +21,11 @@
  * along with ao-payments-all.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.payments.all {
-	// Direct
-	requires transitive com.aoapps.payments.api; // <groupId>com.aoapps</groupId><artifactId>ao-payments-api</artifactId>
-	requires transitive com.aoapps.payments.authorizeNet; // <groupId>com.aoapps</groupId><artifactId>ao-payments-authorizeNet</artifactId>
-	requires transitive com.aoapps.payments.payflowPro; // <groupId>com.aoapps</groupId><artifactId>ao-payments-payflowPro</artifactId>
-	requires transitive com.aoapps.payments.stripe; // <groupId>com.aoapps</groupId><artifactId>ao-payments-stripe</artifactId>
-	requires transitive com.aoapps.payments.test; // <groupId>com.aoapps</groupId><artifactId>ao-payments-test</artifactId>
-	requires transitive com.aoapps.payments.usaepay; // <groupId>com.aoapps</groupId><artifactId>ao-payments-usaepay</artifactId>
+  // Direct
+  requires transitive com.aoapps.payments.api; // <groupId>com.aoapps</groupId><artifactId>ao-payments-api</artifactId>
+  requires transitive com.aoapps.payments.authorizeNet; // <groupId>com.aoapps</groupId><artifactId>ao-payments-authorizeNet</artifactId>
+  requires transitive com.aoapps.payments.payflowPro; // <groupId>com.aoapps</groupId><artifactId>ao-payments-payflowPro</artifactId>
+  requires transitive com.aoapps.payments.stripe; // <groupId>com.aoapps</groupId><artifactId>ao-payments-stripe</artifactId>
+  requires transitive com.aoapps.payments.test; // <groupId>com.aoapps</groupId><artifactId>ao-payments-test</artifactId>
+  requires transitive com.aoapps.payments.usaepay; // <groupId>com.aoapps</groupId><artifactId>ao-payments-usaepay</artifactId>
 }
